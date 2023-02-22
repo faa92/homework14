7cb91ea444ef3b4b7b9b6039f4458a950ac44425
Anton Fedoseev <fedoseev446@gmail.com>
2023-02-22 15:56:34 +0300

    hw

--- src/by/tms/antonfedoseev/homework14/utils/Currency.java
+++ src/by/tms/antonfedoseev/homework14/utils/Currency.java
@@ -1,6 +1,4 @@
 package by.tms.antonfedoseev.homework14.utils;
-
-public class CurrencyEnum {
     public enum Currency {
         BYN("Белорусский рубль"),
         USD("Доллар США"),
@@ -10,11 +8,12 @@ public class CurrencyEnum {
         CNY("Китайский юань");
         private final String rusName;
 
-        Currency( String rusName) {
+        Currency(String rusName) {
             this.rusName = rusName;
         }
 
 
-
     }
-}
+
+
+
--- src/by/tms/antonfedoseev/homework14/utils/ExchangeRate.java
+++ src/by/tms/antonfedoseev/homework14/utils/ExchangeRate.java
@@ -1,5 +1,26 @@
 package by.tms.antonfedoseev.homework14.utils;
 
+import java.math.BigDecimal;
+
 public class ExchangeRate {
+    private Currency currency;
+    private BigDecimal rate;
+
+    public ExchangeRate(Currency currency, BigDecimal rate) {
+
+    }
+
+
+    public ExchangeRate[] getTodayRate() {
+        return new ExchangeRate[]{
+                new ExchangeRate(Currency.USD, new BigDecimal("2,7890"));
+        new ExchangeRate(Currency.EUR, new BigDecimal("2,9789"));
+        new ExchangeRate(Currency.RUS, new BigDecimal("0,037773"));
+        new ExchangeRate(Currency.CNY, new BigDecimal("4,0654"));
+        new ExchangeRate(Currency.GBP, new BigDecimal("3,3538"));
+    };
+
+
+    }
 
 }
